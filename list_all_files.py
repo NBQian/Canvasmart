42162b26ca0e14dacf67149e8dc19fc12c21b64c
Niu BoQian <e0774823@u.nus.edu>
2023-09-13 22:47:55 +0800

    Managed proper display of files and folders

--- list_all_files.py
+++ list_all_files.py
@@ -1,5 +1,7 @@
 import requests
 import json
+from datetime import datetime
+import pytz
 
 # Your Canvas API access token
 token = "21450~ib3PcTIWpRbqQW8FipYOIjYYWtdaxtPWj0YODMYaPzmMilt2kmZCx5OE4Gpe6Gxe"
@@ -11,65 +13,44 @@ base_url = "https://canvas.nus.edu.sg/api/v1"
 headers = {"Authorization": f"Bearer {token}"}
 
 
-# Function to get files for a course
-def get_course_files(course_id):
-    files_url = f"{base_url}/courses/{course_id}/files"
-    response = requests.get(files_url, headers=headers)
-    return json.loads(response.text) if response.status_code == 200 else []
-
-
-# Function to get folders for a course or a parent folder
-def get_folders(parent_type, parent_id):
-    folders_url = f"{base_url}/{parent_type}/{parent_id}/folders"
-    response = requests.get(folders_url, headers=headers)
-    return json.loads(response.text) if response.status_code == 200 else []
-
-
-# Function to get files in a folder
-def get_files(folder_id):
-    files_url = f"{base_url}/folders/{folder_id}/files"
-    response = requests.get(files_url, headers=headers)
-    return json.loads(response.text) if response.status_code == 200 else []
-
-
 # Recursive function to display folders and files
 def display_folders_and_files(parent_type, parent_id, indentation=0):
     folders = get_folders(parent_type, parent_id)
+    if parent_type == "courses":
+        main_folder_id = 0
+        main_folder = None
+        for folder in folders:
+            if folder["name"] == "course files":
+                main_folder_id = folder["id"]
+                main_folder = folder
+                break
+        first_layer_folders = [
+            folder for folder in folders if folder["parent_folder_id"] == main_folder_id
+        ]
+        first_layer_folders.append(main_folder)
+        folders = first_layer_folders
+
     for folder in folders:
-        # print(
-        #     folder["name"],
-        #     folder.get("hidden"),
-        #     folder.get("hidden_for_user"),
-        #     folder.get("lock_at"),
-        # )
-        if folder["files_count"] == 0:
+        if parent_type == "folders" and folder["parent_folder_id"] != parent_id:
+            continue  # Skip if this is not a direct child of the current folder
+        if folder["files_count"] == 0 and folder["folders_count"] == 0:
             continue
-        print(" " * indentation + folder["name"] + ":")
+
+        # Special case for "course files" folder
+        if folder["name"] == "course files":
+            special_indentation = indentation
+        else:
+            folder_name = folder["name"]
+            dash_line = "-" * len(folder_name)
+            print(" " * indentation + " " + folder_name)
+            print(" " * indentation + f"|{dash_line}|")
+            special_indentation = indentation + 4
+
         files = get_files(folder["id"])
         for file in files:
-            print(" " * (indentation + 4) + file["display_name"])
-        display_folders_and_files("folders", folder["id"], indentation + 4)
-
-
-# Function to get courses
-def get_courses():
-    courses_url = f"{base_url}/courses"
-    response = requests.get(courses_url, headers=headers)
-    return json.loads(response.text) if response.status_code == 200 else []
-
-
-# Function to get modules for a course
-def get_modules(course_id):
-    modules_url = f"{base_url}/courses/{course_id}/modules"
-    response = requests.get(modules_url, headers=headers)
-    return json.loads(response.text) if response.status_code == 200 else []
-
-
-# Function to get items in a module
-def get_module_items(course_id, module_id):
-    items_url = f"{base_url}/courses/{course_id}/modules/{module_id}/items"
-    response = requests.get(items_url, headers=headers)
-    return json.loads(response.text) if response.status_code == 200 else []
+            print(" " * special_indentation + file["display_name"])
+        if folder["name"] != "course files":
+            display_folders_and_files("folders", folder["id"], special_indentation)
 
 
 # Main function to display courses and their folders and files
@@ -77,8 +58,10 @@ def main():
     courses = get_courses()
     for course in courses:
         if "name" in course:
-            print(course["name"] + ":")
-
+            course_name = course["name"]
+            equal_line = "=" * len(course_name)
+            print(f"\n{equal_line}\n{course_name}\n{equal_line}")
+            display_folders_and_files("courses", course["id"], indentation=4)
             # Check if there are files in the 'files' section
             course_files = get_course_files(course["id"])
             if course_files:
@@ -94,6 +77,76 @@ def main():
                     for item in items:
                         if item["type"] == "File":
                             print("        " + item["title"])
+
+
+# Function to get paginated data
+def get_paginated_data(url):
+    all_data = []
+    while url:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            all_data.extend(json.loads(response.text))
+            # Check if there is a next page
+            link_header = response.headers.get("Link", None)
+            if link_header:
+                links = link_header.split(",")
+                next_link = [l.split(";")[0] for l in links if 'rel="next"' in l]
+                url = next_link[0][1:-1] if next_link else None
+            else:
+                url = None
+        else:
+            break
+    return all_data
+
+
+def get_course_files(course_id):
+    course_files_url = f"{base_url}/courses/{course_id}/files"
+    return get_paginated_data(course_files_url)
+
+
+# Function to get folders for a course or a parent folder
+def get_folders(parent_type, parent_id):
+    folders_url = f"{base_url}/{parent_type}/{parent_id}/folders"
+    return get_paginated_data(folders_url)
+
+
+# Function to get files in a folder
+def get_files(folder_id):
+    files_url = f"{base_url}/folders/{folder_id}/files"
+    return get_paginated_data(files_url)
+
+
+# Function to get courses
+def get_courses():
+    courses_url = f"{base_url}/courses"
+    all_courses = get_paginated_data(courses_url)
+
+    filtered_courses = []
+    for course in all_courses:
+        if "created_at" in course:
+            course_sem = course["name"][-6:]
+            if course_sem == "[2310]":
+                filtered_courses.append(course)
+
+    return filtered_courses
+
+
+# Function to get modules for a course
+def get_modules(course_id):
+    modules_url = f"{base_url}/courses/{course_id}/modules"
+    return get_paginated_data(modules_url)
+
+
+# Function to get items in a module
+def get_module_items(course_id, module_id):
+    items_url = f"{base_url}/courses/{course_id}/modules/{module_id}/items"
+    return get_paginated_data(items_url)
+
+
+# Function to get files for a course
+def get_course_files(course_id):
+    files_url = f"{base_url}/courses/{course_id}/files"
+    return get_paginated_data(files_url)
 
 
 # Run the main function
--- quicktest.py
+++ quicktest.py
@@ -1,4 +1,7 @@
 import requests
+import json
+from datetime import datetime
+import pytz
 
 # Your Canvas API access token
 token = "21450~ib3PcTIWpRbqQW8FipYOIjYYWtdaxtPWj0YODMYaPzmMilt2kmZCx5OE4Gpe6Gxe"
@@ -10,80 +13,91 @@ base_url = "https://canvas.nus.edu.sg/api/v1"
 headers = {"Authorization": f"Bearer {token}"}
 
 
+# Function to get paginated data
+# Function to get paginated data
+def get_paginated_data(url):
+    all_data = []
+    while url:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            data = json.loads(response.text)
+            all_data.extend(data)
+
+            # Check for pagination
+            link_header = response.headers.get("Link", None)
+            if link_header:
+                links = link_header.split(",")
+                next_link = None
+                for link in links:
+                    if 'rel="next"' in link:
+                        next_link = link.split(";")[0].strip("<>")
+                        break
+                url = next_link
+            else:
+                url = None
+        else:
+            print(f"Failed to get data. Status code: {response.status_code}")
+            break
+    return all_data
+
+
 # Function to get courses
 def get_courses():
     courses_url = f"{base_url}/courses"
-    response = requests.get(courses_url, headers=headers)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(f"Failed to get courses. Status code: {response.status_code}")
-        return []
+    all_courses = get_paginated_data(courses_url)
+
+    # Create a datetime object for June 1, 2023, and make it timezone-aware
+    cutoff_date = datetime.strptime("2023-06-01T00:00:00", "%Y-%m-%dT%H:%M:%S")
+    cutoff_date = pytz.utc.localize(cutoff_date)  # Assuming UTC timezone
+
+    filtered_courses = []
+    for course in all_courses:
+        if "created_at" in course:
+            course_created_at = datetime.fromisoformat(course["created_at"])
+            if course_created_at <= cutoff_date:
+                filtered_courses.append(course)
+
+    return filtered_courses
 
 
-# Function to get files for a folder
-def get_files_in_folder(folder_id):
-    files_url = f"{base_url}/folders/{folder_id}/files"
-    response = requests.get(files_url, headers=headers)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(
-            f"Failed to get files for folder {folder_id}. Status code: {response.status_code}"
-        )
-        return []
-
-
-# Function to get folders for a course
-def get_folders(course_id):
-    folders_url = f"{base_url}/courses/{course_id}/folders"
-    response = requests.get(folders_url, headers=headers)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(
-            f"Failed to get folders for course {course_id}. Status code: {response.status_code}"
-        )
-        return []
-
-
-def get_files_in_courses(course_id):
+# Function to get files for a course
+def get_course_files(course_id):
     files_url = f"{base_url}/courses/{course_id}/files"
-    response = requests.get(files_url, headers)
-    files = response.json()
+    return get_paginated_data(files_url)
 
 
-# Get list of courses
-courses = get_courses()
+def get_course_folders(course_id):
+    folders_url = f"{base_url}/courses/{course_id}/folders"
+    return get_paginated_data(folders_url)
 
-# Loop through each course
-for course in courses:
-    course_id = course.get("id", "N/A")
-    course_name = course.get("name", "N/A")
-    get_files_in_courses(course_id)
-    # Skip courses without a name
-    if not course_name:
-        continue
 
-    print(f"{course_name}")
+def get_course_folders_through_parentID(id):
+    gcftp_url = f"{base_url}/courses/{id}/folders"
+    return get_paginated_data(gcftp_url)
 
-    # Get list of folders for this course
-    folders = get_folders(course_id)
-    print("Folders:")
-    for folder in folders:
-        folder_id = folder.get("id", "N/A")
-        folder_name = folder.get("name", "N/A")
-        print(f"  - {folder_name}")
 
-        print(f"\n{folder.get('files_count')}")
+folders = get_course_folders_through_parentID(45706)
+files = get_course_files(45706)
+# folders = get_course_folders(45706)
 
-        # Get and display files in this folder
-        files_in_folder = get_files_in_folder(folder_id)
-        print(f"    {'File Name':<45} {'Size':<15} {'Created At':<25}")
-        print("    " + "=" * 85)
-        for file in files_in_folder:
-            display_name = file.get("display_name", "N/A")
-            size = file.get("size", "N/A")
-            created_at = file.get("created_at", "N/A")
-            print(f"    {display_name:<45} {size:<15} {created_at:<25}")
-    print("\n")
+for folder in folders:
+    print(
+        folder["name"],
+        folder["files_count"],
+        folder["folders_count"],
+        folder["hidden"],
+        folder["hidden_for_user"],
+    )
+# for file in files:
+#     print(file["filename"])
+# courses = get_courses()
+# for course in courses:
+#     if "name" in course:
+#         print(course["name"], course["id"])
+# #     else:
+# #         continue
+# #     files = get_course_files(course["id"])
+# #     for file in files:
+# #         print(file["filename"])
+# #     print("\n\n")
+# print("nima")
